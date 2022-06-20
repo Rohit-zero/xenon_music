ae478c00c0a023bd5b3bef8265f1ee7ebd44a814
Rohit-zero <Rohitdhakedd39@gmail.com>
2022-06-21 00:58:49 +0530

    1st

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../utils/ai_util.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      body: Stack(
+        children: [
+          VxAnimatedBox()
+              .size(context.screenWidth, context.screenHeight)
+              .withGradient(
+                LinearGradient(
+                  colors: [
+                    AIColors.primaryColor1,
+                    AIColors.primaryColor2,
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              )
+              .make(),
+          AppBar(
+            title: "Xenon Music".text.xl4.bold.wide.make().shimmer(
+                primaryColor: Color.fromARGB(255, 224, 204, 245),
+                secondaryColor: Colors.white),
+            backgroundColor: Colors.transparent,
+            elevation: 0.0,
+            centerTitle: true,
+          ).h(100).p16()
+        ],
+      ),
+    );
+  }
+}
